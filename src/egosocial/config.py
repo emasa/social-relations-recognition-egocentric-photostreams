d850c8f2ab52f19345a09728d27da45122920617
Emanuel Sanchez Aimar <emanuelsanchezaimar@gmail.com>
2018-02-13 20:11:22 +0100

    Add in configuration paths to models ported from Caffe. Update .gitignore with *.h5 (keras format) to avoid tracking models weights.

--- src/egosocial/config.py
+++ src/egosocial/config.py
@@ -16,3 +16,24 @@ CREDENTIALS_FILE = os.path.join(PROJECT_DIR, 'credentials.json.nogit')
 # closest server to get the best latency
 FACE_API_BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face' \
                     '/v1.0/'
+
+MODEL_KEYS = """
+BODY_AGE,BODY_GENDER,BODY_CLOTHING,
+HEAD_AGE,HEAD_GENDER,HEAD_POSE,FACE_APPEARANCE,FACE_EMOTION
+"""
+MODEL_KEYS = [name.strip() for name in MODEL_KEYS.split(',')]
+
+MODEL_FILE_URLS = [
+    ('body_age_trained_on_pipa.h5', None),
+    ('body_gender_trained_on_pipa.h5', None),
+    ('body_clothing_trained_on_berkeleyBodyAttributes.h5', None),
+    ('head_age_trained_on_pipa.h5', None),
+    ('head_gender_trained_on_pipa.h5', None),
+    ('face_pose_trained_on_IMFDB.h5', None),
+    ('face_appearance_trained_on_CelebAFaces.h5', None),
+    ('face_emotion_trained_on_IMFDB.h5', None),
+]
+
+MODELS = {name: MODEL_FILE_URLS[idx] for idx, name in enumerate(MODEL_KEYS)}
+
+MODELS_CACHE_DIR = os.path.join(PROJECT_DIR, 'model_snapshots', 'bins')
