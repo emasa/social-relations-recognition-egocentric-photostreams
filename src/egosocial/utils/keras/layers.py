cff57ef74c9eea470041eba24902edf48949b835
Emanuel Sanchez Aimar <emanuelsanchezaimar@gmail.com>
2018-02-13 20:07:12 +0100

    Add utils/keras/layers/LRN layer required for the AlexNet models exported from Caffe.

---
+++ src/egosocial/utils/keras/layers.py
@@ -0,0 +1,46 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import keras.backend as K
+from keras.layers.core import Layer
+
+
+class LRN(Layer):
+
+    def __init__(self, size=5, alpha=0.0005, beta=0.75, k=2, **kwargs):
+        self.n = size
+        self.alpha = alpha
+        self.beta = beta
+        self.k = k
+        super(LRN, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.shape = input_shape
+        super(LRN, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        half_n = self.n - 1
+        squared = K.square(x)
+        scale = self.k
+        norm_alpha = self.alpha / (2 * half_n + 1)
+        if K.image_dim_ordering() == "th":
+            b, f, r, c = self.shape
+            squared = K.expand_dims(squared, 0)
+            squared = K.spatial_3d_padding(squared, padding=((half_n, half_n), (0, 0), (0,0)))
+            squared = K.squeeze(squared, 0)
+            for i in range(half_n*2+1):
+                scale += norm_alpha * squared[:, i:i+f, :, :]
+        else:
+            b, r, c, f = self.shape
+            squared = K.expand_dims(squared, -1)
+            squared = K.spatial_3d_padding(squared, padding=((0, 0), (0,0), (half_n, half_n)))
+            squared = K.squeeze(squared, -1)
+            for i in range(half_n*2+1):
+                scale += norm_alpha * squared[:, :, :, i:i+f]
+
+        scale = K.pow(scale, self.beta)
+        return x / scale
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
