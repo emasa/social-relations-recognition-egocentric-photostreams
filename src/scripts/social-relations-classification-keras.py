678e1c66a5d553c14e81a2d314347a322e8f207b
Emanuel SA <emanuelsanchezaimar@gmail.com>
2018-02-17 13:49:14 +0000

    Progress made in social relations classification using pre-computed features with a model implemented in Keras

--- src/scripts/social-relations-classification-keras.py
+++ src/scripts/social-relations-classification-keras.py
@@ -5,25 +5,26 @@ import argparse
 import logging
 import os
 import sys
+from os.path import dirname, abspath
+
+sys.path.extend([dirname(dirname(abspath(__file__)))])
 
 import numpy as np
 import scipy
 import sklearn
+import keras
 from keras import backend as K
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.layers import Input, Dense
 from keras.losses import categorical_crossentropy
 from keras.models import Model
 from keras.utils import to_categorical
-from sklearn.preprocessing import StandardScaler
 
 import egosocial.config
-from egosocial.core.types import relation_to_domain_vec
+from egosocial.core.types import relation_to_domain, relation_to_domain_vec
 from egosocial.utils.caffe.misc import load_levelDB_as_array
 from egosocial.utils.keras.autolosses import AutoMultiLossWrapper
 from egosocial.utils.logging import setup_logging
-
-# sys.path.extend([os.path.dirname(os.path.dirname(__file__))])
 
 # constants
 DOMAIN, RELATION = 'domain', 'relation'
@@ -52,11 +53,10 @@ def train_model(conf):
     log.info('Selected attribute(s): {}'.format(attributes_query))
 
     # get data splits composed by selected attributes only
-    data_split = get_data_split(features, selected_attributes,
-                                conf.LABEL_FILES)
-    x_train, x_val, x_test, *labels = data_split
     # preprocess the data
-    x_train, x_val, x_test = preprocess_data(x_train, x_val, x_test)
+    data_split = get_data_split(features, selected_attributes,
+                                conf.LABEL_FILES, preprocess=True)
+    x_train, x_val, x_test, *labels = data_split
     # one-hot encoding for relation
     y_train_rel, y_val_rel, y_test_rel = [
         to_categorical(y, N_CLS_RELATION) for y in labels
@@ -68,11 +68,11 @@ def train_model(conf):
     # discard number of samples
     input_shape = x_train.shape[1:]
     clf_wrapper = AutoMultiLossWrapper(get_model(input_shape))
-    # TODO: move away
-    auto_loss = lambda y_true, y_pred: -K.log(categorical_crossentropy(y_true,
-                                                                       y_pred))
-
-    clf_wrapper.compile(optimizer='adam', loss=auto_loss, loss_weights='auto')
+    clf_wrapper.compile(optimizer='adam',
+                        loss=categorical_crossentropy, 
+                        loss_weights='auto',
+                        #loss_weights=dict(domain=1, relation=2), 
+                        metrics=['accuracy'])
     log.info(clf_wrapper.model.summary())
 
     x_train_inputs = {'attribute_features': x_train}
@@ -90,21 +90,23 @@ def train_model(conf):
                                    'weights.{epoch:02d}-{val_loss:.2f}.h5')
 
     callbacks = [
-        ModelCheckpoint(checkpoint_path, monitor='val_loss',
-                        save_best_only=True),
-        TensorBoard(log_dir='./logs', write_images=True, write_graph=True)
+        #ModelCheckpoint(checkpoint_path, monitor='val_loss',
+      #                 save_best_only=True),
+        ##TensorBoard(log_dir='./logs', write_images=True, write_graph=True)
     ]
 
+    trainable_model = clf_wrapper.model
     log.info("Training model from scratch...")
-    hist = clf_wrapper.model.fit(
+    hist = trainable_model.fit(
         x_train_inputs, y_train_outputs,
         batch_size=batch_size, epochs=epochs,
         validation_data=(x_val_inputs, y_val_outputs),
-        callbacks=callbacks,
+        callbacks=callbacks, verbose=1,
     )
 
-    scores = clf_wrapper.model.evaluate(x_test_inputs, y_test_outputs,
-                                        batch_size=batch_size)
+
+    scores = trainable_model.evaluate(x_test_inputs, y_test_outputs,
+                                      batch_size=batch_size)
 
     log.info(scores)
 
@@ -113,24 +115,133 @@ def get_model(input_shape):
     input_features = Input(shape=input_shape, dtype='float',
                            name='attribute_features')
 
-    relation = Dense(N_CLS_RELATION, activation='linear', name='relation')(
-        input_features)
-    domain = Dense(N_CLS_DOMAIN, activation='softmax', name='domain')(relation)
+    from keras import regularizers
+    from keras.layers.noise import AlphaDropout
+    from keras.layers import Dropout
+
+    x = input_features
+
+#    x = keras.layers.BatchNormalization()(x)
+
+    x = Dense(128, activation='selu', name='dense_1',
+                   bias_initializer='lecun_normal',
+                   kernel_initializer='lecun_normal',
+             )(x)
+    x = AlphaDropout(0.25)(x)
+
+    domain = Dense(N_CLS_DOMAIN, activation='softmax', name='domain',
+                   bias_regularizer=regularizers.l2(0.1),
+                   kernel_regularizer=regularizers.l2(0.1))(x)
+
+    x = keras.layers.concatenate([x, domain])
+    relation = Dense(N_CLS_RELATION, activation='softmax', name='relation',
+                     bias_regularizer=regularizers.l2(0.1),
+                     kernel_regularizer=regularizers.l2(0.1))(x)
 
     clf = Model(inputs=[input_features], outputs=[domain, relation])
 
     return clf
 
+def domain_to_relation_matrix():
+    W = [np.zeros(N_CLS_RELATION) for _ in range(N_CLS_DOMAIN)]
+    for rel in range(N_CLS_RELATION):
+        dom = relation_to_domain(rel)
+        W[dom] += to_categorical(rel, N_CLS_RELATION)
+    return np.array(W).T
 
-def preprocess_data(x_train, x_val, x_test):
+def get_model_2(input_shape):
+    input_features = Input(shape=input_shape, dtype='float',
+                           name='attribute_features')
+
+    x = input_features
+    #x = keras.layers.BatchNormalization()(input_features)
+    #x = keras.layers.Dropout(0.2)(x)
+
+    from keras import regularizers
+
+    relation = Dense(N_CLS_RELATION, activation='softmax', name='relation',
+                     bias_regularizer=regularizers.l2(0.05),
+                     kernel_regularizer=regularizers.l2(0.05)
+                    )(x)
+
+    weights = [domain_to_relation_matrix()]
+
+    domain = Dense(N_CLS_DOMAIN, activation='linear', name='domain', 
+                   use_bias=False, weights=weights, trainable=False)(relation)
+    
+    clf = Model(inputs=[input_features], outputs=[domain, relation])
+
+    return clf
+
+
+def preprocess_data(x_train, x_val, x_test, **kwargs):
+    n_features = x_train.shape[1]
     # normalize data ?
-    normalize = False
+    normalize = True
+    from sklearn.preprocessing import Normalizer, StandardScaler
     if normalize:
+        print('Applying normalization to the data')
+        #scaler = Normalizer(norm='l2').fit(x_train)
         scaler = StandardScaler().fit(x_train)
         x_train = scaler.transform(x_train)
         x_test = scaler.transform(x_test)
         x_val = scaler.transform(x_val)
+    
+    from sklearn.decomposition import PCA
+    import pickle
 
+    dim_reduction = True
+    min_required, max_required = 200, 200
+    min_explained_var = 0.8
+
+    if dim_reduction and n_features > min_required:
+        
+        quantization = False
+        if quantization:
+            # Q promotes sparsity, default
+            Q = kwargs.get('Q', 32)
+            print('Applying Q-sparsity to the data Q={}'.format(Q))
+            x_train = np.floor(Q * x_train)
+            x_test = np.floor(Q * x_test)
+            x_val = np.floor(Q * x_val)
+        
+        pca_conf = kwargs.get('n_components', min_required)
+        if 0 < pca_conf <= 1:
+            print('Computing PCA coefficients')
+            pca = PCA(n_components=pca_conf)
+            pca.fit(x_train)
+        else:
+            # PCA failed, compute again
+            n_components = pca_conf
+            for retry in range(3): # 50, 100, 200
+                print('Computing PCA retry {}'.format(retry+1))
+                pca = PCA(n_components=n_components, svd_solver='randomized')
+                pca.fit(x_train)
+                
+                explained_var = np.sum(pca.explained_variance_ratio_)
+                if not np.isnan(explained_var) and explained_var < min_explained_var and n_components < max_required:
+                    n_components *= 2
+                else:
+                    break
+
+        explained_var = np.sum(pca.explained_variance_ratio_)
+        n_components = pca.n_components_
+
+        backup_pca = False
+        if backup_pca:
+            filename = os.path.join(egosocial.config.MODELS_CACHE_DIR, 
+                                'pca_exp_var_{}_Q_{}.b'.format(explained_var, Q))
+            print('Backing up PCA coefficients in {}.'.format(filename))
+            with open(filename, 'wb') as f:
+                 pickle.dump(pca, f)
+
+        print('Applying PCA to the data with explained variance {} dims {}'.format(explained_var, n_components))
+        x_train = pca.transform(x_train)
+        x_test = pca.transform(x_test)
+        x_val = pca.transform(x_val)
+    else:
+        print('Skip dimensionality reduction')
+ 
     return x_train, x_val, x_test
 
 
@@ -157,20 +268,6 @@ class AttributeSelector:
 
     def filter_by_keyword(self, attribute_list, key):
         return [attr_name for attr_name in attribute_list if key in attr_name]
-
-
-def get_model_configurations(all_attributes):
-    # extend list of attributes
-    extended_attrs = ['all', 'body', 'face'] + all_attributes
-
-    # some attributes are splitted in two files (one for each person)
-    # create a list unique attributes name
-    unique_attrs = set(
-        [attr[:-2] if attr.endswith('_1') or attr.endswith('_2') else attr
-         for attr in extended_attrs])
-
-    return [item for item in sorted(unique_attrs)]
-
 
 class Configuration:
     def __init__(self, args):
@@ -223,7 +320,7 @@ class Configuration:
         self.SAVE_STATS = args.save_stats
 
 
-def get_data_split(attribute_features, selected_attributes, label_files):
+def get_data_split_old(attribute_features, selected_attributes, label_files):
     # concatenate attributes
     fused_features, labels = {}, {}
     for split in ['train', 'test', 'eval']:
@@ -245,6 +342,41 @@ def get_data_split(attribute_features, selected_attributes, label_files):
     y_val = labels['test']
 
     return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_data_split(attribute_features, selected_attributes, label_files, 
+                   preprocess=False):
+    # splits (switch from caffe's split name convention to keras's convention)
+    _train, _val, _test = 'train', 'test', 'eval'
+    if preprocess:
+        from collections import defaultdict
+        features = defaultdict(dict)
+        # preprocess selected attributes individually
+        for attr_name in selected_attributes:
+             print('Preprocessing attribute: {}'.format(attr_name))
+             data = preprocess_data( attribute_features[_train][attr_name]
+                                   , attribute_features[_val][attr_name]
+                                   , attribute_features[_test][attr_name])
+             for split_idx, split in enumerate((_train, _val, _test)):
+                 features[split][attr_name] = data[split_idx]
+    else:
+        features = attribute_features
+    
+    # concatenate attributes
+    fused_features, labels = {}, {}
+    for split in (_train, _val, _test):
+        selected_features = [features[split][attr_name]
+                             for attr_name in selected_attributes]
+    
+        fused_features[split] = np.concatenate(selected_features, axis=1)
+
+        with open(label_files[split]) as file_label_list:
+            labels[split] = np.array([file_label.split()[1]
+                                      for file_label in file_label_list],
+                                     dtype=np.int)
+    
+    result = [fused_features[split] for split in (_train, _val, _test)]
+    result.extend([labels[split] for split in (_train, _val, _test)])
+    return result
 
 
 def get_features(conf):
@@ -400,8 +532,8 @@ def save_features(attribute_features, features_dir, compressed=True):
 def load_features(features_dir):
     attribute_features = {split: {} for split in ['train', 'test', 'eval']}
 
-    for numpy_file in os.listdir(features_dir):
-        # Split the extension from the path and normalise it to lowercase.
+    for numpy_file in sorted(os.listdir(features_dir)):
+        # Split the extension from the path and normalize it to lowercase.
         filename, ext = os.path.splitext(numpy_file)
         ext = ext.lower()
 
@@ -417,7 +549,20 @@ def load_features(features_dir):
             continue
 
         attr_name, split = filename.rsplit('_', 1)
-        attribute_features[split][attr_name] = features
+
+        # some attributes are splitted in two files (one for each person)
+        # create a list unique attributes name
+        if attr_name.endswith('_1') or attr_name.endswith('_2'):
+            attr_name = attr_name[:-2]
+
+        if attr_name in attribute_features[split]:
+            array = np.concatenate([attribute_features[split][attr_name], 
+                                    features.astype('float32', copy=False)],
+                                   axis=1)
+        else:
+            array = features.astype('float32', copy=False)
+        
+        attribute_features[split][attr_name] = array
 
         print("Loading {}...".format(features_path))
 
