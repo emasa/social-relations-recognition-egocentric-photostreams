38116776292c66ff00068cfdf8a1bb86ec1054e1
Emanuel Sanchez Aimar <emanuelsanchezaimar@gmail.com>
2018-02-19 01:37:19 +0100

    Add function to limit memory allocation with Tensorflow backend (keras).

---
+++ src/egosocial/utils/keras/backend.py
@@ -0,0 +1,14 @@
+
+import tensorflow as tf
+import keras.backend as K
+
+
+def limit_gpu_allocation_tensorflow(memory_percentage=0.25):
+
+    assert 0 <= memory_percentage <= 1
+
+    # default: allocates 25% of total memory
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = memory_percentage
+    tf_session = tf.Session(config=config)
+    K.set_session(tf_session)
