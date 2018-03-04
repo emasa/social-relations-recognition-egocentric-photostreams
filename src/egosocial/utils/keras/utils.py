4c1a17f501f140d9fe5e110689318282b257743c
Emanuel SA <emanuelsanchezaimar@gmail.com>
2018-03-04 12:41:04 +0000

    Performance improvements in fuse_inputs_generator

--- src/egosocial/utils/keras/utils.py
+++ src/egosocial/utils/keras/utils.py
@@ -1,6 +1,9 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import deque
+from multiprocessing.dummy import Pool as ThreadPool
+
 from keras.preprocessing.image import ImageDataGenerator
 
 from ..concurrency import threadsafe_generator
@@ -14,12 +17,24 @@ def fuse_inputs_generator(generators, inputs, outputs_callback=None):
     if outputs_callback is None: # indices 0: first input, 1: label data
         outputs_callback = lambda batch, _inputs: batch[0][1]
     
-    while True:
-        data_batch = [next(gen) for gen in generators]
-        # multiple inputs
-        X = {input_name : data_batch[idx][0] for idx, input_name in enumerate(inputs)}        
-
-        yield X, outputs_callback(data_batch, inputs)
+    n_batches = 3
+    n_gens = len(generators)
+    tasks = list(generators) * n_batches
+    workers = len(tasks)
+    
+    queue = deque(maxlen=n_batches)
+    with ThreadPool(workers) as pool:
+        while True:
+            if len(queue) == 0:
+                batches = pool.map(next, tasks)
+                for idx in range(n_batches):
+                    data_batch = batches[idx * n_gens : (idx+1) * n_gens]
+                    queue.append(data_batch)
+            
+            data_batch = queue.popleft()            
+            # multiple inputs
+            X = {input_name : data_batch[idx][0] for idx, input_name in enumerate(inputs)}
+            yield X, outputs_callback(data_batch, inputs)
 
 
 def flow_from_dirs(input_directories, fix_path_cbk=None, **kwargs):
