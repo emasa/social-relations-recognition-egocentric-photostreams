22f227ba52533dcb4bd804af9b0972c0e4f675ed
Emanuel Sanchez Aimar <emauelsanchezaimar@gmail.com>
2018-05-18 17:32:52 +0200

    Memory alloc feature.

--- src/egosocial/utils/keras/backend.py
+++ src/egosocial/utils/keras/backend.py
@@ -5,12 +5,17 @@ import tensorflow as tf
 import keras.backend as K
 
 
-def limit_gpu_allocation_tensorflow(memory_percentage=0.25):
+def limit_gpu_allocation_tensorflow(memory_percentage=None):
 
-    assert 0 <= memory_percentage <= 1
-
-    # default: allocates 25% of total memory
     config = tf.ConfigProto()
-    config.gpu_options.per_process_gpu_memory_fraction = memory_percentage
+
+    if memory_percentage is not None:
+        # fixed amount of gpu memory (across all gpus)
+        assert 0 <= memory_percentage <= 1
+        config.gpu_options.per_process_gpu_memory_fraction = memory_percentage
+    else:
+        # on demand
+        config.gpu_options.allow_growth = True
+
     tf_session = tf.Session(config=config)
     K.set_session(tf_session)
